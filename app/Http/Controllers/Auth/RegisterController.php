ea6878f202a7821748ee83d1ec708d81106c163c
JorgeLuises <161666613+JorgeLuises@users.noreply.github.com>
2025-09-17 17:01:47 -0600

    Creación de migraciones en laravel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RegisterController extends Controller
 {
     public function index () {
         return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|min:2|max:35',
+            'username' => 'required|min:2|max:20|unique:users',
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,26 +11,47 @@
         </div>
 
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
-            <form action="">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase font-bold"> Nombre </label>
-                    <input type="text" name="name" id="name" placeholder="Ingresa tu nombre" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+                    <input type="text" name="name" id="name" placeholder="Ingresa tu nombre" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none
+                    @error('name') border-red-500 @enderror" value="{{ old('name') }}">
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase font-bold"> Usuario </label>
-                    <input type="text" name="username" id="username" placeholder="Ingresa tu nombre de usuario" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+                    <input type="text" name="username" id="username" placeholder="Ingresa tu nombre de usuario" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none
+                    @error('username') border-red-500 @enderror" value="{{ old('username') }}">
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase font-bold"> Correo electrónico </label>
-                    <input type="email" name="email" id="email" placeholder="Ingresa tu correo electrónico" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+                    <input type="email" name="email" id="email" placeholder="Ingresa tu correo electrónico" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none
+                    @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password" class="mb-2 block uppercase font-bold"> Contraseña </label>
-                    <input type="password" name="password" id="password" placeholder="Crea una contraseña" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+                    <input type="password" name="password" id="password" placeholder="Crea una contraseña" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none
+                    @error('password') border-red-500 @enderror">
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password_confirmation" class="mb-2 block uppercase font-bold"> Repite contraseña </label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repetir contraseña" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repetir contraseña" class="border rounded-lg p-3 w-full focus:ring-1 focus:ring-blue-500 focus:border-blue-500 focus:outline-none
+                    @error('password_confirmation') border-red-500 @enderror">
+                    @error('password_confirmation')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <input type="submit" value="Crear Cuenta" class="border rounded-lg w-full bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold text-white p-3">
